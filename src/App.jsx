a0b1dcf6121d979cb74bb91eb45766255dfe9eed
Sergey Shulga <shul.sergio@gmail.com>
2024-09-19 03:35:47 +0300

    v 1.4 fix trobles + add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,24 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import "./App.css";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactForm from "./components/ContactForm/ContactForm";
 import Section from "./components/Section/Section";
+import Container from "./components/Container/Container";
 // const userData = [];
 function App() {
-  // const [itemsContact, setItemsContact] = useState(() => {
-  //   const lsItems = window.localStorage.getItem("contact");
-  //   console.log("savedItems-", lsItems);
-  //   console.log("JSON.parse(savedItems)-", JSON.parse(lsItems));
-  //   if (lsItems !== null) {
-  //     return JSON.parse(lsItems);
-  //   } else {
-  //     return userData;
-  //   }
-  // });
-  // useEffect(() => {
-  //   window.localStorage.setItem("contact", JSON.stringify(itemsContact));
-  // }, [itemsContact]);
-
-  const [filter, setFilter] = useState("");
-
-  // const deleteContact = (contactId) => {
-  //   console.log(contactId);
-  //   setItemsContact((prevContact) => {
-  //     return prevContact.filter((item) => item.id !== contactId);
-  //   });
-  // };
-
-  // const visibleContacts = itemsContact.filter((item) =>
-  //   item.name.toLowerCase().includes(filter.toLowerCase())
-  // );
-
   return (
     <>
       <Section>
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <SearchBox filter={filter} onFilter={setFilter} />
-        <ContactList />
+        <Container>
+          <h1>Phonebook</h1>
+          <ContactForm />
+          <SearchBox
+          // filter={filter} onFilter={setFilter}
+          />
+          <ContactList />
+        </Container>
       </Section>
     </>
   );
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,8 +3,8 @@ export default function Contact({ item, onDelete }) {
   return (
     <div className={css.contactAll}>
       <div className={css.contactsBox}>
-        <li>{item.name}</li>
-        <li>{item.number}</li>{" "}
+        <li className={css.name}>{item.name}</li>
+        <li className={css.number}>{item.number}</li>{" "}
       </div>
       <button className={css.btnDel} onClick={() => onDelete(item.id)}>
         Delete
